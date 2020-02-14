c8a39bde2d2d36420f68381ae2af0a733a40929b
Marina Polyakowa <polyakowamarina@gmail.com>
2020-02-14 08:11:05 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 import Numbers from './components/NumberVerify';
 
-const numMusk = '+7(985)0II-**-**';
+const numMask = '+7(985)0II-**-**';
 
-Numbers({ mask: numMusk });
+Numbers({ mask: numMask });
