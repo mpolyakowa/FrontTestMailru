9367c7d252fef74f7bce532f673bcf7960c6a48c
Marina Polyakowa <polyakowamarina@gmail.com>
2020-02-14 07:28:53 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
